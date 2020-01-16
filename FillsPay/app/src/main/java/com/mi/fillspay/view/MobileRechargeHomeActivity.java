bba7caccc7521420055035b83a9882149aea6bfd
marasamallikarjuna <marasamallikarjuna@gamil.com>
2020-01-16 11:01:26 +0530

    mobile recharge screens included

--- FillsPay/app/src/main/java/com/mi/fillspay/view/MobileRechargeHomeActivity.java
+++ FillsPay/app/src/main/java/com/mi/fillspay/view/MobileRechargeHomeActivity.java
@@ -13,33 +13,11 @@ import com.mi.fillspay.adapter.SampleAdapter;
 
 public class MobileRechargeHomeActivity extends AppCompatActivity {
 
-    ViewGroup root;
-    RecyclerView list;
-
-    private CircleLayoutManager layoutManager;
-    private SampleAdapter adapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mobile_recharg_main);
 
-        root = (ViewGroup)findViewById(R.id.root);
-        list = (RecyclerView)findViewById(R.id.recycler_view);
-
-        adapter = new SampleAdapter(this);
-        final int radius = (int) getResources().getDimension(R.dimen.list_radius);
-        final int peek = (int) getResources().getDimension(R.dimen.list_peek);
-        layoutManager = new CircleLayoutManager(this,
-                CircleLayoutManager.Gravity.START,
-                CircleLayoutManager.Orientation.VERTICAL,
-                radius,
-                peek,
-                true);
-
-        list.setLayoutManager(layoutManager);
-        list.setAdapter(adapter);
-        adapter.notifyDataSetChanged();
 
         findViewById(R.id.icon_reg).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,6 +27,4 @@ public class MobileRechargeHomeActivity extends AppCompatActivity {
         });
 
     }
-
-
 }
